9ba70e764997f0ed22c0920b8320efe881c6b753
vv.grigorev <vv.grigorev@tensor.ru>
2022-04-21 18:37:14 +0300

    warm start?

--- model.py
+++ model.py
@@ -46,7 +46,7 @@ from utils.distributed import apply_gradient_allreduce
 from utils import utils as utl
 
 # TODO import whole tacotron2
-from Tacotron2 import tacotron2, tacotron2_common
+import Tacotron2
 
 class LocationLayer(nn.Module):
     def __init__(self, attention_n_filters, attention_kernel_size, attention_dim, initscheme="xavier_uniform"):
@@ -776,9 +776,23 @@ def load_model(hparams, distributed_run=False):
         model = apply_gradient_allreduce(model)
 
     # TODO Сделать флаг для загрузки/незагрузки пре-трейна
+    def warm_start_model(checkpoint_path, model, ignore_layers):
+        assert os.path.isfile(checkpoint_path)
+        print("Warm starting model from checkpoint '{}'".format(checkpoint_path))
+        checkpoint_dict = torch.load(checkpoint_path, map_location='cpu')
+        model_dict = checkpoint_dict['state_dict']
+        if len(ignore_layers) > 0:
+            model_dict = {k: v for k, v in model_dict.items()
+                          if k not in ignore_layers}
+            dummy_dict = model.state_dict()
+            dummy_dict.update(model_dict)
+            model_dict = dummy_dict
+        model.load_state_dict(model_dict)
+        return model
+
     checkpoint_path = "/content/drive/MyDrive/SBIS_schemes/checkpoint_tacotron"
     ignore_layers = ''
-    warm_model = tacotron2.train.warm_start_model(checkpoint_path, model, ignore_layers)
+    warm_model = warm_start_model(checkpoint_path, model, ignore_layers)
     return warm_model
 
 
